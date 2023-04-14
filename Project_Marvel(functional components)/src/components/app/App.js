3228e0992de00e742153e3e4a60d18eb714db789
Aleksei <lisiy.81@gmail.comm>
2023-04-14 20:20:35 +0300

    react0019

--- Project_Marvel(functional components)/src/components/app/App.js
+++ Project_Marvel(functional components)/src/components/app/App.js
@@ -6,6 +6,7 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from "../../resources/img/vision.png";
 
@@ -20,18 +21,20 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        {/* <ErrorBoundary> */}
-        <RandomChar />
-        {/* </ErrorBoundary> */}
+        <ErrorBoundary>
+          <RandomChar />
+        </ErrorBoundary>
         <div className="char__content">
-          {/* <ErrorBoundary> */}
-          {/* <CharList onCharSelected={onCharSelected} /> */}
-          <CharInfo charId={selectedChar} />
-          {/* </ErrorBoundary> */}
+          <ErrorBoundary>
+            <CharList onCharSelected={onCharSelected} />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
         </div>
         <img className="bg-decoration" src={decoration} alt="vision" />
-        {/* <AppBanner />
-        <ComicsList /> */}
+        <AppBanner />
+        <ComicsList />
       </main>
     </div>
   );
