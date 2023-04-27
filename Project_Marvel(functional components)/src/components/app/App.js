c7787dc82347f150590f1a1b3b36eb44c961747a
Aleksei <lisiy.81@gmail.comm>
2023-04-27 13:52:39 +0300

    react0022

--- Project_Marvel(functional components)/src/components/app/App.js
+++ Project_Marvel(functional components)/src/components/app/App.js
@@ -1,9 +1,13 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
 
-import { MainPage, ComicsPage, Page404 } from "../pages";
-import SingleComicPage from "../pages/SingleComicPage";
+const Page404 = lazy(() => import("../pages/404"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
 
 const App = () => {
   return (
@@ -11,20 +15,22 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Switch>
-            <Route exact path="/">
-              <MainPage />
-            </Route>
-            <Route exact path="/comics">
-              <ComicsPage />
+          <Suspense fallback={<Spinner />}>
+            <Switch>
+              <Route exact path="/">
+                <MainPage />
+              </Route>
+              <Route exact path="/comics">
+                <ComicsPage />
+              </Route>
               <Route exact path="/comics/:comicId">
                 <SingleComicPage />
               </Route>
-            </Route>
-            <Route path="*">
-              <Page404 />
-            </Route>
-          </Switch>
+              <Route path="*">
+                <Page404 />
+              </Route>
+            </Switch>
+          </Suspense>
         </main>
       </div>
     </Router>
--- Project_Marvel(functional components)/src/components/appHeader/AppHeader.js
+++ Project_Marvel(functional components)/src/components/appHeader/AppHeader.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import { MainPage, ComicsPage } from "../pages";
+
 import "./appHeader.scss";
 
 const AppHeader = () => {
