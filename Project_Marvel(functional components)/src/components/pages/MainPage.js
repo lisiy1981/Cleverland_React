4391f0b2478ffc2cc58da3113b7ad646855d752f
Aleksei <lisiy.81@gmail.comm>
2023-05-12 16:02:15 +0300

    react0035

--- Project_Marvel(functional components)/src/components/pages/MainPage.js
+++ Project_Marvel(functional components)/src/components/pages/MainPage.js
@@ -4,6 +4,7 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import CharSearchForm from "../charSearchForm/CharSearchForm";
 
 import decoration from "../../resources/img/vision.png";
 
@@ -23,6 +24,9 @@ const MainPage = () => {
         </ErrorBoundary>
         <ErrorBoundary>
           <CharInfo charId={selectedChar} />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <CharSearchForm />
         </ErrorBoundary>
       </div>
       <img className="bg-decoration" src={decoration} alt="vision" />
